b9ad85c572176b69494a2b46839fe48fbbf98845
zahra-aghayeva <zehra.aqayeva777@gmail.com>
2026-04-28 18:26:36 +0000

    Add task 17: poly_integral

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -10,15 +10,13 @@ def poly_integral(poly, C=0):
     """
     if not isinstance(poly, list) or len(poly) == 0:
         return None
-    if not isinstance(C, int) and not isinstance(C, float):
+    if not isinstance(C, (int, float)):
         return None
     
     for coeff in poly:
         if not isinstance(coeff, (int, float)):
             return None
 
-    # İnteqral sabiti C siyahının ilk elementidir (x^0)
-    # Tam ədəd olub olmadığını yoxlayırıq
     if isinstance(C, float) and C.is_integer():
         C = int(C)
     
@@ -27,13 +25,11 @@ def poly_integral(poly, C=0):
     for i in range(len(poly)):
         new_coeff = poly[i] / (i + 1)
         
-        # Əgər ədəd .0 ilə bitirsə (məsələn 5.0), onu int edirik
         if new_coeff.is_integer():
             new_coeff = int(new_coeff)
         
         integral.append(new_coeff)
 
-    # Siyahının sonundakı artıq sıfırları təmizləyirik
     while len(integral) > 1 and integral[-1] == 0:
         integral.pop()
 
