2eaec559defecc2e6c795d5686a1ef8dd0081aca
zahra-aghayeva <zehra.aqayeva777@gmail.com>
2026-05-20 16:14:18 +0000

    Implement task 6 cat_arrays for list concatenation

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Defines a function to concatenate two arrays."""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenates two arrays of ints/floats into a new list.
+
+    Args:
+        arr1 (list): The first list.
+        arr2 (list): The second list.
+
+    Returns:
+        list: A completely new list containing elements of both arrays.
+    """
+    return arr1 + arr2
