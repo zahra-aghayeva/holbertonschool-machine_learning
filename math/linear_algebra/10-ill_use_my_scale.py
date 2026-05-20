f4347f81f0f3384a5c70409d45c3db92a55ebb68
zahra-aghayeva <zehra.aqayeva777@gmail.com>
2026-05-20 18:06:33 +0000

    Implement task 10 np_shape using numpy ndarray shape attribute

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Defines a function to calculate the shape of a numpy.ndarray."""
+
+
+def np_shape(matrix):
+    """Calculates the shape of a numpy.ndarray.
+
+    Args:
+        matrix (numpy.ndarray): The input numpy array.
+
+    Returns:
+        tuple: The shape of the matrix as a tuple of integers.
+    """
+    return matrix.shape
