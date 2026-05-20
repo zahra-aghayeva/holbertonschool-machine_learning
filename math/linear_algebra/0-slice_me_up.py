7d442b64eefdfb7fe914fcc236310e4fa6bd9885
zahra-aghayeva <zehra.aqayeva777@gmail.com>
2026-05-20 15:45:48 +0000

    Implement task 0 using exact required layout and 8 lines constraint

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Slice a 2D array."""
-import numpy as np
-arr = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [11, 12, 13, 14, 15, 16, 17, 18, 19, 20], [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]])
-arr1, arr2, arr3 = arr[:2, :], arr[:, -5:], arr[1:3, 5:7]
-print(arr1)
-print(arr2)
-print(arr3)
+arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
+arr1 = arr[:2]
+arr2 = arr[-5:]
+arr3 = arr[1:6]
+print("The first two numbers of the array are: {}".format(arr1))
+print("The last five numbers of the array are: {}".format(arr2))
+print("The 2nd through 6th numbers of the array are: {}".format(arr3))
